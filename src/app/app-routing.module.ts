949c94dfe864ef3a0db4dd8a98605ce8e2ae7b5f
FOXFOXYS <foxfoxy.pro@gmail.com>
2020-08-07 10:59:45 +0200

    Ajout et Modif du App Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 // Import des Components 
-import {ListRecipeComponent } from './page/list-recipe/list-recipe.component';
 import {HomeComponent} from './page/home/home.component';
+import {ListRecipeComponent } from './page/list-recipe/list-recipe.component';
+import {ViewRecipeComponent} from './page/view-recipe/view-recipe.component';
 import {PageNotFoundComponent} from './page/page-not-found/page-not-found.component';
  
 // Routes pour les Components
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'recipes', component: ListRecipeComponent},
+  { path: 'recipes/:id', component: ViewRecipeComponent},
   { path: 'not-found', component: PageNotFoundComponent, pathMatch: 'full', },
   { path: '**', redirectTo: 'not-found' }
 
