219be9215239839539cd3dfcb72aa87cede68a11
FOXFOXYS <foxfoxy.pro@gmail.com>
2020-08-07 10:45:31 +0200

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+// Import des Components 
+import {ListRecipeComponent } from './page/list-recipe/list-recipe.component';
+import {HomeComponent} from './page/home/home.component';
+ 
+// Routes pour les Components
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'recipes', component: ListRecipeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListRecipeComponent } from './page/list-recipe/list-recipe.component';
+import { ListRecipeModule } from './page/list-recipe/list-recipe.module';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { HomeComponent } from './page/home/home.component';
+import { HomeModule } from './page/home/home.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListRecipeComponent,
+    RecipeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ListRecipeModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
