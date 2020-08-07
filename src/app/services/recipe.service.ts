200ec706ffa783680b54b2f80b1c0b549deed8c6
FOXFOXYS <foxfoxy.pro@gmail.com>
2020-08-07 15:01:48 +0200

    Ajout des Subject

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import {environment} from 'src/environments/environment';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  recipe: any[] = [];
+  recipeSubject = new Subject<any[]>();
+  recipes: any[] = [];
   constructor(
     private httpClient: HttpClient
   ) { }
 
+
+  emitRecipeSubject(){
+    this.recipeSubject.next(this.recipes.slice());
+  }
+
   getAllRecipes() {
     this.httpClient.get(environment.apiUrl+"recipes").subscribe(
       (recipes) => {
+        
         console.log(recipes);
       },
       (error) => {
